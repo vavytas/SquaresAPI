e2a29b67f673c95dea7175f4332cfe22df0fd331
Vytas Vadapolas <vadapolas.vytas@gmail.com>
2024-03-24 16:34:39 +0200

    Add DataContext. Add needed packages.

---
+++ SquaresAPIHomework/SquaresAPIHomework/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SquaresAPIHomework.Entities;
+
+namespace SquaresAPIHomework.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<OnePoint> Points { get; set; }
+
+    }
+}

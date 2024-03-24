440726d4efd5fe814c86f978749695b03b0f6da4
Vytas Vadapolas <vadapolas.vytas@gmail.com>
2024-03-24 16:10:26 +0200

    Initial commit, Add OnePoint class.

---
+++ SquaresAPIHomework/SquaresAPIHomework/Entities/OnePoint.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SquaresAPIHomework.Entities
+{
+    public class OnePoint
+    {
+        [Key]
+        public int PointId { get; set; }
+
+        public double XProp { get; set; }
+        public double YProp { get; set; }
+
+        public OnePoint(int pointId, double xprop, double yprop)
+        {
+            this.PointId = pointId;
+            this.XProp = xprop;
+            this.YProp = yprop;
+        }
+
+        public OnePoint(double xprop, double yprop)
+        {
+            this.XProp = xprop;
+            this.YProp = yprop;
+        }
+
+        public OnePoint()
+        {
+        }
+
+    }
+}
